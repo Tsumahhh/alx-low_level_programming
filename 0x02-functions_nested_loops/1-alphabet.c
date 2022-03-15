ed3312335810038df10b95894b95ced0ba151302
Tsumahhh <solotsuma@gmail.com>
2022-03-15 10:26:58 -0700

    Boy

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+
+/**
+
+ * print_alphabet - prints the alphabet, in lowercase, followed by a new line
+
+ */
+
+void print_alphabet(void)
+
+{
+
+	char ch;
+
+
+
+	ch = 'a';
+
+
+
+	while (ch <= 'z')
+
+	{
+
+		_putchar(ch);
+
+		ch++;
+
+	}
+
+
+
+	_putchar('\n');
+
+}
