ca08a76bdbf072fa46b477164cb4c37d6e05a7ab
Tsumahhh <99472574+Tsumahhh@users.noreply.github.com>
2022-03-15 20:39:53 +0300

    Update 1-alphabet.c
    Toto

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,40 +1,5 @@
 #include "main.h"
 
-<<<<<<< HEAD
-
-/**
-
- * print_alphabet - prints the alphabet, in lowercase, followed by a new line
-
- */
-
-void print_alphabet(void)
-
-{
-
-	char ch;
-
-
-
-	ch = 'a';
-
-
-
-	while (ch <= 'z')
-
-	{
-
-		_putchar(ch);
-
-		ch++;
-
-	}
-
-
-
-	_putchar('\n');
-
-=======
 /**
  * print_alphabet - prints the alphabet, in lowercase, followed by a new line
  */
@@ -51,5 +16,4 @@ void print_alphabet(void)
 	}
 
 	_putchar('\n');
->>>>>>> 84e3730735c52ff7ff342f65bb4716a3101046a8
 }
