168b2a9e869c8feb04747072b41e519d9d7279c7
Tsumahhh <solotsuma@gmail.com>
2022-03-15 06:03:52 -0700

    Toboa

---
+++ 0x02-functions_nested_loops/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - main prints putchar
+ * follwed by a new line
+ * return :0
+ */
+
+int main(void)
+
+{
+
+
+
+		printf("_putchar\n");
+
+
+
+			system("pause.0");
+
+}
+
+
+
+
