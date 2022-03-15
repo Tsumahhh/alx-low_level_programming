0ff1762e499817a74a9ae013b79d665815d7a160
Tsumahhh <99472574+Tsumahhh@users.noreply.github.com>
2022-03-15 20:05:29 +0300

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,22 @@
-include "main.h"
+#include "main.h"
+
 /**
- * main - Entry point
- *
- * this program prints _putchar folloewd by a new line
- * Return; Alway 0
- */
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
 	_putchar('_');
-	_putchhar('p');
+	_putchar('p');
 	_putchar('u');
 	_putchar('t');
-	_putchar('C');
+	_putchar('c');
 	_putchar('h');
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
+
+
 	return (0);
 }
-
