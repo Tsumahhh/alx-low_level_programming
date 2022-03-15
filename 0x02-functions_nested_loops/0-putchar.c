a28a508f893136af8593cd6b11b9c2a693fcae2d
Tsumahhh <solotsuma@gmail.com>
2022-03-15 10:19:45 -0700

    Boy

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,43 @@
-include "main.h"
-/**
- * main - Entry point
- *
- * this program prints _putchar folloewd by a new line
- * Return; Alway 0
- */
-int main(void)
-{
-	_putchar('_');
-	_putchhar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('C');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-	return (0);
-}
+#include "main.h"
 
+
+
+/**
+
+* main - Entry point
+
+*
+
+* Return: Always 0 (Success)
+
+*/
+
+int main(void)
+
+{
+
+	_putchar('_');
+
+	_putchar('p');
+
+	_putchar('u');
+
+	_putchar('t');
+
+	_putchar('c');
+
+	_putchar('h');
+
+	_putchar('a');
+
+	_putchar('r');
+
+	_putchar('\n');
+
+
+
+
+
+	return (0);
+
+}
