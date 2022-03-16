d77d9c06a52169dd3a00a186be39f88498dbd0ee
Tsumahhh <99472574+Tsumahhh@users.noreply.github.com>
2022-03-16 18:40:02 +0300

    Cool

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,33 +1,19 @@
-uth: gathama
-
+/*
+ * File: 6-abs.c
+ * Auth: Solomon Mbotia
  */
-
 #include "main.h"
-
- /**
-  *
-  *  * _abs - computes the absolute value of an integer
-  *
-  *   * @n: The integer to be computed
-  *
-  *    *
-  *
-  *     * Return: Returns n if n>=0, else -n
-  *
-  *      */
-
- int _abs(int n)
-
+/**
+ * _abs - computes the absolute value of an integer
+ * @n: The integer to be computed
+ *
+ * Return: Returns n if n>=0, else -n
+ */
+int _abs(int n)
 {
-
-		if (n >= 0)
-
-					return (n);
-
-			else
-
-						return (-n);
-
-
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
 
 }
