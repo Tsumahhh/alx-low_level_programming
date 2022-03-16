0f157831ed5a1f53f2ae14ca9e8f2eecbbd9377d
Tsumahhh <99472574+Tsumahhh@users.noreply.github.com>
2022-03-16 18:08:12 +0300

    Bob

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,35 +1,18 @@
 /*
- *
- *  * File: 4-isalpha.c
- *
- *   * Auth: gathama
- *
- *    */
-
+ * File: 4-isalpha.c
+ * Auth: gathama
+ */
 #include "main.h"
-
 /**
+ * _isalpha - checks for alphabetic character.
+ * @c: character to be checked
  *
- *  * _isalpha - checks for alphabetic character.
- *
- *   * @c: character to be checked
- *
- *    *
- *
- *     * Return: Returns 1 if c is a letter, otherwise 0
- *
- *      */
-
+ * Return: Returns 1 if c is a letter, otherwise 0
+ */
 int _isalpha(int c)
-
 {
-
-		if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-
-					return (1);
-
-			else
-
-						return (0);
-
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
 }
