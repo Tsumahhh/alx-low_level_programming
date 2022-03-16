a8bce00ff0715f50cf2abc8a7dd8773a2e7dfb68
Tsumahhh <99472574+Tsumahhh@users.noreply.github.com>
2022-03-16 19:08:29 +0300

    Smooth

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,51 +1,26 @@
-/*
- *
- *  * File: 8-24_hours.c
- *
- *   * Auth: gathama
- *
- *    */
-
+*
+ * File: 8-24_hours.c
+ * Auth: Solomon Mbotia
+ */
 #include "main.h"
-
 /**
- *
- *  * jack_bauer - prints every minute of the day of Jack Bauer,
- *
- *   * starting from 00:00 to 23:59.
- *
- *    */
-
+ * jack_bauer - prints every minute of the day of Jack Bauer,
+ * starting from 00:00 to 23:59.
+ */
 void jack_bauer(void)
-
 {
+	int h, m;
 
-		int h, m;
-
-
-
-			for (h = 0; h <= 23; h++)
-
-					{
-
-								for (m = 0; m <= 59; m++)
-
-											{
-
-															_putchar((h / 10) + '0');
-
-																		_putchar((h % 10) + '0');
-
-																					_putchar(':');
-
-																								_putchar((m / 10) + '0');
-
-																											_putchar((m % 10) + '0');
-
-																														_putchar('\n');
-
-																																}
-
-									}
-
+	for (h = 0; h <= 23; h++)
+	{
+		for (m = 0; m <= 59; m++)
+		{
+			_putchar((h / 10) + '0');
+			_putchar((h % 10) + '0');
+			_putchar(':');
+			_putchar((m / 10) + '0');
+			_putchar((m % 10) + '0');
+			_putchar('\n');
+		}
+	}
 }
